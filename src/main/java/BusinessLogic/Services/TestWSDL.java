a8dcbf7c3751e0c9019194e5236ef06a02f06ae0
oscar26 <deangel95@gmail.com>
2015-11-09 23:19:25 -0500

    Web service test complete.

--- src/main/java/BusinessLogic/Services/TestWSDL.java
+++ src/main/java/BusinessLogic/Services/TestWSDL.java
@@ -1,22 +1,17 @@
-
 package BusinessLogic.Services;
 
-import BusinessLogic.Controllers.DrugsController;
 import DataAccess.Entities.Drug;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 
-/**
- *
- * @author oscar
- */
 @WebService(serviceName = "TestWSDL")
 public class TestWSDL {
     
     @WebMethod(operationName = "testTransaction")
-    public Drug testTransaction(Long drugId) {
-        DrugsController drugsController = new DrugsController();
-        Drug drug = drugsController.findDrugById(drugId);
+    public Drug testTransaction(@WebParam(name = "drugId") Long drugId) {
+        DrugService drugService = new DrugService();
+        Drug drug = drugService.findDrugById(drugId);
         System.out.println(drug);
         return drug;
     }
